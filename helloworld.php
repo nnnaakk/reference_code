df8474a6725788e4d06cc2509cf2d843635605b6
nnnaakk <77379434+nnnaakk@users.noreply.github.com>
2021-01-15 16:20:45 +0900

    Create helloworld.php

---
+++ helloworld.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <p>html初めまして、福島です。</p>
+
+    <?php
+        echo "<h2>php初めまして、福島です。</h2>";
+        print "<div>php初めまして、福島です。</div>";
+
+    ?>
+
+</body>
+</html>
